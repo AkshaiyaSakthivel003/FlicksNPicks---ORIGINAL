086477c9f57a79c4e9f23f792ebddcadd954c8cf
Akshaiya Sakthivel <120248925+AkshaiyaSakthivel003@users.noreply.github.com>
2025-07-17 18:26:34 +0400

    Add files via upload

---
+++ Day10/src/Sidebar.js
@@ -0,0 +1,17 @@
+// src/components/Sidebar.js
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Users</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
