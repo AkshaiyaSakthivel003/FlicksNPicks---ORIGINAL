19ab7b810522e32d6a982e07f2066865d73a8d33
Akshaiya Sakthivel <120248925+AkshaiyaSakthivel003@users.noreply.github.com>
2025-07-17 18:56:34 +0400

    Delete DAY 16 directory

--- DAY 16/AdminFlix/src/main/java/com/example/flix/exception/DataNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.flix.exception;
-
-public class DataNotFoundException extends RuntimeException
-{
-    public DataNotFoundException(Long id)
-    {
-        super("Could not found the user with id "+ id);
-    }
-}
